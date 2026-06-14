c0d7a54c5480be776ca7eefaa8c9b1e328fab85c
Sean Li <9913851+saifahn@users.noreply.github.com>
2026-06-14 10:39:03 +0900

    Resolve merge conflict in p0p1_entries migration
    Chain from m1n2o3p4q5r6 (current head after master merge).

--- alembic/versions/b2c3d4e5f6g7_p0p1_entries_table.py
+++ alembic/versions/b2c3d4e5f6g7_p0p1_entries_table.py
@@ -1,7 +1,7 @@
 """Create p0p1_entries table with RLS
 
 Revision ID: b2c3d4e5f6g7
-Revises: a1b2c3d4e5f6
+Revises: m1n2o3p4q5r6
 Create Date: 2026-06-13
 
 RLS-protected table for the P0P1 voting contest. Each authenticated user
@@ -17,7 +17,7 @@ from alembic import op
 
 
 revision: str = "b2c3d4e5f6g7"
-down_revision: Union[str, None] = "a1b2c3d4e5f6"
+down_revision: Union[str, None] = "m1n2o3p4q5r6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
